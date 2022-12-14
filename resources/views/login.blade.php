83440fa8dc64f5687e59a092e0f31b65e61a6acd
=wahyu09999 <=2041720191@student.polinema.ac.id>
2022-12-14 23:27:26 +0700

    fix UI

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -51,7 +51,7 @@
               <div class="d-flex justify-content-center py-4">
                 <a href="index.html" class="logo d-flex align-items-center w-auto">
                   <img src="" alt="">
-                  <span class="d-none d-lg-block">Admin</span>
+                  <span class="d-none d-lg-block">Admin Login Page</span>
                 </a>
               </div><!-- End Logo -->
 
@@ -60,8 +60,8 @@
                 <div class="card-body">
 
                   <div class="pt-4 pb-2">
-                    <h5 class="card-title text-center pb-0 fs-4">Login</h5>
-                    <p class="text-center small">Enter your username & password to login</p>
+                    <h5 class="card-title text-center pb-0 fs-4"></h5>
+                    <p class="text-center small"></p>
                   </div>
 
                   <form class="row g-3 needs-validation" method="POST" action="{{route('masuk')}}">
@@ -99,7 +99,7 @@
                 <!-- You can delete the links only if you purchased the pro version. -->
                 <!-- Licensing information: https://bootstrapmade.com/license/ -->
                 <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+                Designed by <a href="https://bootstrapmade.com/"></a>
               </div>
 
             </div>

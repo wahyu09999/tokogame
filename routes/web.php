b2557262432921181503f3833b29c352c17b599d
=wahyu09999 <=2041720191@student.polinema.ac.id>
2022-12-14 17:44:52 +0700

    2

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ use Illuminate\Support\Facades\Artisan;
 //     return view('page.home');
 // });
 Route::get('/mysql', function () {
-    // Artisan::call('migrate:rollback', ['--force' => true]);
+    Artisan::call('migrate:rollback', ['--force' => true]);
     Artisan::call('migrate:fresh', ['--force' => true]);
     Artisan::call('db:seed', ['--force' => true]);
 });
